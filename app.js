ca97c8c232dc80374dd1d353433515cebc695ce7
Dheeraj Bhati <dheeraj.bhati@tavant.com>
2016-08-18 16:18:09 +0530

    test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-   
+    console.log("GET /");
     var sql = require("mssql");
 
     // config for your database
@@ -14,15 +14,16 @@ app.get('/', function (req, res) {
     };
 
     // connect to your database
-    sql.connect(config, function (err) {
-    
+    sql.connect("mssql://loladmin:SitL0Lpass@lolsitdataserver.database.windows.net/lolsitdatabase?encrypt=true", function (err) {
+        console.log("Connection Started");
         if (err) console.log(err);
 
+		console.log("Connection Done");
         // create Request object
         var request = new sql.Request();
-           
+        console.log("Query Started");
         // query to the database and get the records
-        request.query('select * from Student', function (err, recordset) {
+        request.query('select * from SITTable', function (err, recordset) {
             
             if (err) console.log(err)
 
@@ -30,9 +31,14 @@ app.get('/', function (req, res) {
             res.send(recordset);
             
         });
+		console.log("Query No No");
     });
 });
 
+app.get('/test', function (req, res) {
+   
+ res.send("hello");
+});
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 });
